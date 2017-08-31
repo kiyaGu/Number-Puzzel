acd709c14f51ebaf0e43fdff525207d888e63bf9
KiyaG <hamilee.kiya@gmail.com>
2017-08-31 12:22:40 +0100

    make the game detect winners

--- script.js
+++ script.js
@@ -62,15 +62,42 @@ let drawNumberImg = function(dx, dy, i) {
     });
 }
 
-let findNeibours = function(puzzelEntryNumber) {
-    let x = puzzelEntryNumber.x;
-    let y = puzzelEntryNumber.y;
 
-
-}
 puzzelEntryNumbers.forEach((element) => {
     drawNumberImg(element.position.x, element.position.y, element.puzzelNumber);
 })
+let checkPosition = function(index) {
+    if (positions[index].positionNumber == index && positions[index].currentPlaceHolder == index + 1)
+        return true;
+}
+let checkGameResult = function() {
+    let test = positions.reduce((preTest, position) => {
+        return preTest && checkPosition(positions.indexOf(position))
+    });
+    return test;
+    // if (puzzelEntryNumbers[0].position.x == 0 & puzzelEntryNumbers[1].position.y == 0)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[1].position.x == 125 & puzzelEntryNumbers[1].position.y == 0)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[2].position.x == 250 & puzzelEntryNumbers[2].position.y == 0)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[3].position.x == 0 & puzzelEntryNumbers[3].position.y == 101)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[4].position.x == 125 & puzzelEntryNumbers[4].position.y == 101)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[5].position.x == 250 & puzzelEntryNumbers[5].position.y == 101)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[6].position.x == 0 & puzzelEntryNumbers[6].position.y == 202)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[7].position.x == 125 & puzzelEntryNumbers[7].position.y == 202)
+    //     puzzelEntryNumbers.status = true;
+    // if (puzzelEntryNumbers[0].status && puzzelEntryNumbers[1].status &&
+    //     puzzelEntryNumbers[2].status && puzzelEntryNumbers[3].status &&
+    //     puzzelEntryNumbers[4].status && puzzelEntryNumbers[5].status &&
+    //     puzzelEntryNumbers[6].status && puzzelEntryNumbers[7].status
+    // )
+    //     return true;
+}
 canvas.addEventListener('click', (event) => {
     let position = getMousePos(event);
     //1
@@ -83,6 +110,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[0].status = false;
             puzzelEntryNumbers[0].currentLocation = 1;
             positions[1].currentPlaceHolder = positions[0].currentPlaceHolder;
+            // puzzelEntryNumbers[0].position = positions[1];
+            positions[0].currentPlaceHolder = 9;
         } else if (positions[3].availablity) {
             drawNumberImg(positions[3].x, positions[3].y, positions[0].currentPlaceHolder);
             repaintPosition(positions[0].x, positions[0].y);
@@ -91,6 +120,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[0].status = false;
             puzzelEntryNumbers[0].currentLocation = 3;
             positions[3].currentPlaceHolder = positions[0].currentPlaceHolder;
+            // puzzelEntryNumbers[0].position = positions[3];
+            positions[0].currentPlaceHolder = 9;
         }
     }
     //2
@@ -103,6 +134,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[1].status = false;
             puzzelEntryNumbers[1].currentLocation = 0;
             positions[0].currentPlaceHolder = positions[1].currentPlaceHolder;
+            // puzzelEntryNumbers[1].position = positions[0];
+            positions[1].currentPlaceHolder = 9;
         } else if (positions[2].availablity) {
             drawNumberImg(positions[2].x, positions[2].y, positions[1].currentPlaceHolder);
             repaintPosition(positions[1].x, positions[1].y);
@@ -111,6 +144,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[1].status = false;
             puzzelEntryNumbers[1].currentLocation = 2;
             positions[2].currentPlaceHolder = positions[1].currentPlaceHolder;
+            // puzzelEntryNumbers[1].position = positions[2];
+            positions[1].currentPlaceHolder = 9;
         } else if (positions[4].availablity) {
             drawNumberImg(positions[4].x, positions[4].y, positions[1].currentPlaceHolder);
             repaintPosition(positions[1].x, positions[1].y);
@@ -119,6 +154,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[1].status = false;
             puzzelEntryNumbers[1].currentLocation = 4;
             positions[4].currentPlaceHolder = positions[1].currentPlaceHolder;
+            // puzzelEntryNumbers[1].position = positions[4];
+            positions[1].currentPlaceHolder = 9;
         }
     }
     //3
@@ -131,6 +168,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[2].status = false;
             puzzelEntryNumbers[2].currentLocation = 1;
             positions[1].currentPlaceHolder = positions[2].currentPlaceHolder;
+            // puzzelEntryNumbers[2].position = positions[1];
+            positions[2].currentPlaceHolder = 9;
         } else if (positions[5].availablity) {
             drawNumberImg(positions[5].x, positions[5].y, positions[2].currentPlaceHolder);
             repaintPosition(positions[2].x, positions[2].y);
@@ -139,6 +178,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[2].status = false;
             puzzelEntryNumbers[2].currentLocation = 5;
             positions[5].currentPlaceHolder = positions[2].currentPlaceHolder;
+            // puzzelEntryNumbers[2].position = positions[5];
+            positions[2].currentPlaceHolder = 9;
         }
     }
     //4
@@ -151,6 +192,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[3].status = false;
             puzzelEntryNumbers[3].currentLocation = 0;
             positions[0].currentPlaceHolder = positions[3].currentPlaceHolder;
+            // puzzelEntryNumbers[3].position = positions[0];
+            positions[3].currentPlaceHolder = 9;
         } else if (positions[4].availablity) {
             drawNumberImg(positions[4].x, positions[4].y, positions[3].currentPlaceHolder);
             repaintPosition(positions[3].x, positions[3].y);
@@ -159,6 +202,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[3].status = false;
             puzzelEntryNumbers[3].currentLocation = 4;
             positions[4].currentPlaceHolder = positions[3].currentPlaceHolder;
+            // puzzelEntryNumbers[3].position = positions[4];
+            positions[3].currentPlaceHolder = 9;
         } else if (positions[6].availablity) {
             drawNumberImg(positions[6].x, positions[6].y, positions[3].currentPlaceHolder);
             repaintPosition(positions[3].x, positions[3].y);
@@ -167,6 +212,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[3].status = false;
             puzzelEntryNumbers[3].currentLocation = 6;
             positions[6].currentPlaceHolder = positions[3].currentPlaceHolder;
+            // puzzelEntryNumbers[3].position = positions[6];
+            positions[3].currentPlaceHolder = 9;
         }
     }
     //5
@@ -179,6 +226,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[4].status = false;
             puzzelEntryNumbers[4].currentLocation = 1;
             positions[1].currentPlaceHolder = positions[4].currentPlaceHolder;
+            // puzzelEntryNumbers[4].position = positions[1];
+            positions[4].currentPlaceHolder = 9;
         } else if (positions[3].availablity) {
             drawNumberImg(positions[3].x, positions[3].y, positions[4].currentPlaceHolder);
             repaintPosition(positions[4].x, positions[4].y);
@@ -187,6 +236,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[4].status = false;
             puzzelEntryNumbers[4].currentLocation = 3;
             positions[3].currentPlaceHolder = positions[4].currentPlaceHolder;
+            // puzzelEntryNumbers[4].position = positions[3];
+            positions[4].currentPlaceHolder = 9;
         } else if (positions[5].availablity) {
             drawNumberImg(positions[5].x, positions[5].y, positions[4].currentPlaceHolder);
             repaintPosition(positions[4].x, positions[4].y);
@@ -195,6 +246,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[4].status = false;
             puzzelEntryNumbers[4].currentLocation = 5;
             positions[5].currentPlaceHolder = positions[4].currentPlaceHolder;
+            // puzzelEntryNumbers[4].position = positions[5];
+            positions[4].currentPlaceHolder = 9;
         } else if (positions[7].availablity) {
             drawNumberImg(positions[7].x, positions[7].y, positions[4].currentPlaceHolder);
             repaintPosition(positions[4].x, positions[4].y);
@@ -203,6 +256,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[4].status = false;
             puzzelEntryNumbers[4].currentLocation = 7;
             positions[7].currentPlaceHolder = positions[4].currentPlaceHolder;
+            // puzzelEntryNumbers[4].position = positions[7];
+            positions[4].currentPlaceHolder = 9;
         }
     }
     //6
@@ -215,6 +270,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[5].status = false;
             puzzelEntryNumbers[5].currentLocation = 2;
             positions[2].currentPlaceHolder = positions[5].currentPlaceHolder;
+            // puzzelEntryNumbers[5].position = positions[2];
+            positions[5].currentPlaceHolder = 9;
         } else if (positions[4].availablity) {
             drawNumberImg(positions[4].x, positions[4].y, positions[5].currentPlaceHolder);
             repaintPosition(positions[5].x, positions[5].y);
@@ -223,6 +280,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[5].status = false;
             puzzelEntryNumbers[5].currentLocation = 4;
             positions[4].currentPlaceHolder = positions[5].currentPlaceHolder;
+            // puzzelEntryNumbers[5].position = positions[4];
+            positions[5].currentPlaceHolder = 9;
         } else if (positions[8].availablity) {
             if (positions[8].currentPlaceHolder == 9) {
                 positions[8].currentPlaceHolder = 6;
@@ -234,6 +293,8 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[5].status = false;
             puzzelEntryNumbers[5].currentLocation = 8;
             positions[8].currentPlaceHolder = positions[5].currentPlaceHolder;
+            // puzzelEntryNumbers[5].position = positions[8];
+            positions[5].currentPlaceHolder = 9;
         }
     }
     //7
@@ -246,6 +307,7 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[6].status = false;
             puzzelEntryNumbers[6].currentLocation = 3;
             positions[3].currentPlaceHolder = positions[6].currentPlaceHolder;
+            positions[6].currentPlaceHolder = 9;
         } else if (positions[7].availablity) {
             drawNumberImg(positions[7].x, positions[7].y, positions[6].currentPlaceHolder);
             repaintPosition(positions[6].x, positions[6].y);
@@ -254,6 +316,7 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[6].status = false;
             puzzelEntryNumbers[6].currentLocation = 7;
             positions[7].currentPlaceHolder = positions[6].currentPlaceHolder;
+            positions[6].currentPlaceHolder = 9;
         }
     }
     //8
@@ -266,25 +329,28 @@ canvas.addEventListener('click', (event) => {
             puzzelEntryNumbers[7].status = false;
             puzzelEntryNumbers[7].currentLocation = 4;
             positions[4].currentPlaceHolder = positions[7].currentPlaceHolder;
+            positions[7].currentPlaceHolder = 9;
         } else if (positions[6].availablity) {
             drawNumberImg(positions[6].x, positions[6].y, positions[7].currentPlaceHolder);
             repaintPosition(positions[7].x, positions[7].y);
             positions[7].availablity = true;
             positions[6].availablity = false;
             puzzelEntryNumbers[7].status = false;
-            puzzelEntryNumbers[7].currentLocation = positions[7].currentPlaceHolder;
-            positions[6].currentPlaceHolder = 8;
+            puzzelEntryNumbers[7].currentLocation = 6;
+            positions[6].currentPlaceHolder = positions[7].currentPlaceHolder;
+            positions[7].currentPlaceHolder = 9;
         } else if (positions[8].availablity) {
-            if (positions[7].currentPlaceHolder == 9) {
-                positions[7].currentPlaceHolder = 8;
+            if (positions[8].currentPlaceHolder == 9) {
+                positions[8].currentPlaceHolder = 8;
             }
-            drawNumberImg(positions[8].x, positions[8].y, 8, positions[7].currentPlaceHolder);
+            drawNumberImg(positions[8].x, positions[8].y, positions[7].currentPlaceHolder);
             repaintPosition(positions[7].x, positions[7].y);
             positions[7].availablity = true;
             positions[8].availablity = false;
             puzzelEntryNumbers[7].status = false;
             puzzelEntryNumbers[7].currentLocation = 8;
             positions[8].currentPlaceHolder = positions[7].currentPlaceHolder;
+            positions[7].currentPlaceHolder = 9;
         }
     }
     //9
@@ -295,12 +361,18 @@ canvas.addEventListener('click', (event) => {
             positions[8].availablity = true;
             positions[5].availablity = false;
             positions[5].currentPlaceHolder = positions[8].currentPlaceHolder;
+            positions[8].currentPlaceHolder = 9;
         } else if (positions[7].availablity) {
             drawNumberImg(positions[7].x, positions[7].y, positions[8].currentPlaceHolder);
             repaintPosition(positions[8].x, positions[8].y);
             positions[8].availablity = true;
             positions[7].availablity = false;
             positions[7].currentPlaceHolder = positions[8].currentPlaceHolder;
+            positions[8].currentPlaceHolder = 9;
         }
     }
+    if (checkGameResult())
+        setTimeout(() => {
+            alert("Congratulation, you won...HURRAH!!!");
+        }, 100);
 })
